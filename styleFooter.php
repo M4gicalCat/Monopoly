aee53816c88bc0ccae7336fde8d4b3d43f0d485e
M4gicalCat <philippefaisandier@gmail.com>
2021-02-07 18:01:02 +0100

    Le style du footer ^^'

---
+++ styleFooter.php
@@ -0,0 +1,25 @@
+<style>
+  #rectFooter{
+    width:100%;
+    padding:5%;
+    margin-top: 5%;
+    margin-bottom: 0%;
+    background-color: #10102F;
+    color:#AABBAA;
+  }
+  #insideDiv{
+    font-family: sans-serif;
+  }
+  #DEP{
+    padding-top:1%;
+    width: 240px;
+    height: 110px;
+  }
+  #IUT{
+    padding-left: 10px;
+    padding-top: 1%;
+    margin-top: 1px;
+    width: 190px;
+    height: 90px;
+  }
+</style>

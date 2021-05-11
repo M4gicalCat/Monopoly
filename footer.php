e65b0fbe40178a3a4f2498f1f4463b631d65e3d6
M4gicalCat <philippefaisandier@gmail.com>
2021-05-11 13:53:17 +0200

    last update

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
     <div id"insideDiv">
       Philippe Faisandier<br>
       DUT informatique<br>
-      <a href="http://www.iut-bm.univ-fcomte.fr/"><img id="IUT" src="IUT_LOGO.png"></a>
-      <a href="http://www.iut-bm.univ-fcomte.fr/"><img id="DEP" src="DPT_LOGO.png"></a>
+      <a href="http://www.iut-bm.univ-fcomte.fr/"><img id="IUT" src="./IUT_LOGO.png"></a>
+      <a href="http://www.iut-bm.univ-fcomte.fr/"><img id="DEP" src="./DPT_LOGO.png"></a>
     </div>
   </div>
 </footer>
